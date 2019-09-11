dbd38c01a5ff7c9b8bc1c03cf879ea18637d0df5
李炅倫 <openbox008@gmail.com>
2019-09-11 10:16:00 +0800

    email

---
+++ email.py
@@ -0,0 +1,13 @@
+import re
+
+email_text = """
+Big Data Analytics/ Deep LearningSocial Computing / Computational Social Science / Crowdsourcing
+Multimediaand Network SystemsQuality of ExperienceInformation SecurityPh.D. candidate at NTU EEchihfan02-27883799#1602Camera CalibrationComputer VisionData
+Analysiscmchang02-27883799#1671System OptimizationMachine LearningyusraBig data
+analysiscclin02-27883799#1668Data Analysisrusi02-27883799#1668Government Procurement ActFinancial
+Managementkatekuen02-27883799#1602AdministrationEvent Planningseanyu02-27883799#1668Data 
+AnalysisPsychology & NeuroscienceMarketingxinchinchenEmbedded Systemkyoyachuan062602-27883799
+#1601FinTechActuarial ScienceData Analysiskai0604602-27883799#1601Data AnalysisMachine Learningchloe02-27839427Accountingafun02-27883799 afun@iis.sinica.edu.tw
+#1673Data AnalysisWeb developmentyunhsu198902-27883799#1668MarketingTIGP Ph.D. Fellow at Academia Sinica & NCCUbaowalyMachine LearningData AnalysisSocial Computingchangyc1427883799#1678
+Data Analysisjimmy1592302-2788379 jimmy15923@iis.sinica.com.tw#1688Data AnalysisjasontangAnalysisMachine Learninguchen02-27883799#1668Deep Learningpjwu02-27883799#1604Computational PhotographyData Analysis
+"""
